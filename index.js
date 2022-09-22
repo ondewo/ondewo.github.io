a99cb7ba88cc8019af58a8c16718d74ee66c6b40
Ahmed Hasanovic <ahmed.hasanovic@ondewo.com>
2022-09-22 16:55:06 +0200

    First draft of new design layout

---
+++ index.js
@@ -0,0 +1,48 @@
+import * as ondewo from './data.js';
+
+function setupPage() {
+	for (let tech of ondewo.ondewo.technology) {
+		const divTemp = `
+            <div class="d-flex border border-primary flex-row justify-content-center rounded flex-nowrap p-2 m-3">
+			<div class="d-flex flex-column w-50">
+				<alert style="cursor: pointer;" onclick="openLink('${tech.product_link}')" class="alert bg-secondary text-light w-75 font-weight-bold">ONDEWO ${tech.name}</alert>
+				<p>Clients SDKs on Github for</p>
+				<div class="d-flex flex-row flex-nowrap">
+					<img
+						class="tech-logo"
+						onclick="openLink('${tech.clients.python}')"
+						src="${ondewo.ondewo.logos.python_logo}"
+						alt="Python Logo"
+					/>
+					<img class="tech-logo" onclick="openLink('${tech.clients.angular}')" src="${ondewo.ondewo.logos.angular_logo}" alt="Angular Logo" />
+					<img class="tech-logo" onclick="openLink('${tech.clients.nodejs}')" src="${ondewo.ondewo.logos.nodejs_logo}" alt="Nodejs Logo" />
+					<img class="tech-logo" onclick="openLink('${tech.clients.typescript}')" src="${ondewo.ondewo.logos.typescript_logo}" alt="Typescript Logo" />
+					<img class="tech-logo" onclick="openLink('${tech.clients.js}')" src="${ondewo.ondewo.logos.js_logo}" alt="Javascript Logo" />
+				</div>
+			</div>
+			<div class="d-flex flex-column w-50 ">
+				<p class="font-weight-bold">Version Documentation</p>
+				<div id="${tech.name}-versions" class="d-flex flex-wrap w-100 bg-red">
+				</div>
+			</div>
+		</div>
+        `;
+		document.getElementById('main-container').innerHTML += divTemp;
+		for (let version of tech.versions) {
+			const buttonsTemp = `
+						<button onclick="openVersionLink('${version.link}')" class="btn btn-secondary m-1 p-1">${version.number}</button>
+        `;
+			document.getElementById(`${tech.name}-versions`).innerHTML += buttonsTemp;
+		}
+	}
+}
+
+window.openLink = (url) => {
+	window.open(url);
+};
+
+window.openVersionLink = (url) => {
+	window.open(`https://htmlpreview.github.io/?${url}`);
+};
+
+setupPage();
