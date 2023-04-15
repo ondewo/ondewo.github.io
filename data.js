3d3b0a8fa4b79189520cb1afab9263cbbec74a44
Andreas Rath <andreas.rath@ondewo.com>
2023-04-15 11:06:38 +0200

    Fixed code formatting style

--- data.js
+++ data.js
@@ -1,140 +1,146 @@
 const ondewo = {
-  technology: [
-    {
-      name: 'NLU',
-      product_link:
-        'https://ondewo.com/en/products/natural-language-understanding/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-nlu-client-python',
-        angular: 'https://github.com/ondewo/ondewo-nlu-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-nlu-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-nlu-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-nlu-client-js',
-      },
-      versions: [
-        { number: '4.6.0', link: 'ondewo-nlu-api/4.6.0/' },
-        { number: '4.5.0', link: 'ondewo-nlu-api/4.5.0/' },
-        { number: '4.4.0', link: 'ondewo-nlu-api/4.4.0/' },
-        { number: '4.3.0', link: 'ondewo-nlu-api/4.3.0/' },
-        { number: '4.2.0', link: 'ondewo-nlu-api/4.2.0/' },
-        { number: '4.1.0', link: 'ondewo-nlu-api/4.1.0/' },
-        { number: '4.0.0', link: 'ondewo-nlu-api/4.0.0/' },
-        { number: '3.5.2', link: 'ondewo-nlu-api/3.5.2/' },
-        { number: '3.5.1', link: 'ondewo-nlu-api/3.5.1/' },
-        { number: '3.5.0', link: 'ondewo-nlu-api/3.5.0/' },
-        { number: '3.4.0', link: 'ondewo-nlu-api/3.4.0/' },
-        { number: '3.3.0', link: 'ondewo-nlu-api/3.3.0/' },
-        { number: '3.2.0', link: 'ondewo-nlu-api/3.2.0/' },
-        { number: '3.1.0', link: 'ondewo-nlu-api/3.1.0/' },
-        { number: '3.0.0', link: 'ondewo-nlu-api/3.0.0/' },
-        { number: '2.15.0', link: 'ondewo-nlu-api/2.15.0/' },
-        { number: '2.14.0', link: 'ondewo-nlu-api/2.14.0/' },
-        { number: '2.13.0', link: 'ondewo-nlu-api/2.13.0/' },
-      ],
-      end: '',
+    technology: [
+        {
+            name: 'NLU',
+            product_link:
+                'https://ondewo.com/en/products/natural-language-understanding/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-nlu-client-python',
+                angular: 'https://github.com/ondewo/ondewo-nlu-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-nlu-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-nlu-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-nlu-client-js',
+            },
+            versions: [
+                {number: '4.6.0', link: 'ondewo-nlu-api/4.6.0/'},
+                {number: '4.5.0', link: 'ondewo-nlu-api/4.5.0/'},
+                {number: '4.4.0', link: 'ondewo-nlu-api/4.4.0/'},
+                {number: '4.3.0', link: 'ondewo-nlu-api/4.3.0/'},
+                {number: '4.2.0', link: 'ondewo-nlu-api/4.2.0/'},
+                {number: '4.1.0', link: 'ondewo-nlu-api/4.1.0/'},
+                {number: '4.0.0', link: 'ondewo-nlu-api/4.0.0/'},
+                {number: '3.5.2', link: 'ondewo-nlu-api/3.5.2/'},
+                {number: '3.5.1', link: 'ondewo-nlu-api/3.5.1/'},
+                {number: '3.5.0', link: 'ondewo-nlu-api/3.5.0/'},
+                {number: '3.4.0', link: 'ondewo-nlu-api/3.4.0/'},
+                {number: '3.3.0', link: 'ondewo-nlu-api/3.3.0/'},
+                {number: '3.2.0', link: 'ondewo-nlu-api/3.2.0/'},
+                {number: '3.1.0', link: 'ondewo-nlu-api/3.1.0/'},
+                {number: '3.0.0', link: 'ondewo-nlu-api/3.0.0/'},
+                {number: '2.15.0', link: 'ondewo-nlu-api/2.15.0/'},
+                {number: '2.14.0', link: 'ondewo-nlu-api/2.14.0/'},
+                {number: '2.13.0', link: 'ondewo-nlu-api/2.13.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'S2T',
+            product_link: 'https://ondewo.com/en/products/speech-to-text/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-s2t-client-python',
+                angular: 'https://github.com/ondewo/ondewo-s2t-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-s2t-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-s2t-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-s2t-client-js',
+            },
+            versions: [
+                {number: '5.2.0', link: 'ondewo-s2t-api/5.2.0/'},
+                {number: '5.0.0', link: 'ondewo-s2t-api/5.0.0/'},
+                {number: '4.0.0', link: 'ondewo-s2t-api/4.0.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'T2S',
+            product_link: 'https://ondewo.com/en/products/text-to-speech/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-t2s-client-python',
+                angular: 'https://github.com/ondewo/ondewo-t2s-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-t2s-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-t2s-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-t2s-client-js',
+            },
+            versions: [
+                {number: '5.0.0', link: 'ondewo-t2s-api/5.0.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'SIP',
+            product_link:
+                'https://ondewo.com/en/products/voip-telephone-system-integration/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-sip-client-python',
+                angular: 'https://github.com/ondewo/ondewo-sip-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-sip-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-sip-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-sip-client-js',
+            },
+            versions: [
+                {number: '4.0.0', link: 'ondewo-sip-api/4.0.0/'},
+                {number: '3.3.0', link: 'ondewo-sip-api/3.3.0/'},
+                {number: '3.2.0', link: 'ondewo-sip-api/3.2.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'VTSI',
+            product_link:
+                'https://ondewo.com/en/products/voip-telephone-system-integration/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-vtsi-client-python',
+                angular: 'https://github.com/ondewo/ondewo-vtsi-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-vtsi-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-vtsi-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-vtsi-client-js',
+            },
+            versions: [
+                {number: '5.0.0', link: 'ondewo-vtsi-api/5.0.0/'},
+                {number: '4.0.0', link: 'ondewo-vtsi-api/4.0.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'CSI',
+            product_link:
+                'https://ondewo.com/en/products/voip-telephone-system-integration/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-csi-client-python',
+                angular: 'https://github.com/ondewo/ondewo-csi-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-csi-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-csi-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-csi-client-js',
+            },
+            versions: [
+                {number: '3.0.0', link: 'ondewo-csi-api/3.0.0/'},
+            ],
+            end: '',
+        },
+        {
+            name: 'SURVEY',
+            product_link: 'https://ondewo.com/en/products/call-centre-ai/',
+            clients: {
+                python: 'https://github.com/ondewo/ondewo-survey-client-python',
+                angular: 'https://github.com/ondewo/ondewo-survey-client-angular',
+                nodejs: 'https://github.com/ondewo/ondewo-survey-client-nodejs',
+                typescript: 'https://github.com/ondewo/ondewo-survey-client-typescript',
+                js: 'https://github.com/ondewo/ondewo-survey-client-js',
+            },
+            versions: [
+                {number: '2.0.0', link: 'ondewo-survey-api/2.0.0/'},
+            ],
+            end: '',
+        },
+    ],
+    logos: {
+        python_logo:
+            'https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/files/python-logo-only.svg',
+        angular_logo: 'https://angular.io/assets/images/logos/angular/angular.svg',
+        typescript_logo: 'https://cdn-icons-png.flaticon.com/512/5968/5968381.png',
+        nodejs_logo:
+            'https://t3.ftcdn.net/jpg/03/52/67/82/240_F_352678266_NFcwIwhhY76mkQItT4lCxyxcCTP3LgvY.jpg',
+        js_logo: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png',
     },
-    {
-      name: 'S2T',
-      product_link: 'https://ondewo.com/en/products/speech-to-text/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-s2t-client-python',
-        angular: 'https://github.com/ondewo/ondewo-s2t-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-s2t-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-s2t-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-s2t-client-js',
-      },
-      versions: [
-        { number: '5.2.0', link: 'ondewo-s2t-api/5.2.0/' },
-        { number: '5.0.0', link: 'ondewo-s2t-api/5.0.0/' },
-        { number: '4.0.0', link: 'ondewo-s2t-api/4.0.0/' },
-      ],
-      end: '',
-    },
-    {
-      name: 'T2S',
-      product_link: 'https://ondewo.com/en/products/text-to-speech/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-t2s-client-python',
-        angular: 'https://github.com/ondewo/ondewo-t2s-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-t2s-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-t2s-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-t2s-client-js',
-      },
-      versions: [{ number: '5.0.0', link: 'ondewo-t2s-api/5.0.0/' }],
-      end: '',
-    },
-    {
-      name: 'SIP',
-      product_link:
-        'https://ondewo.com/en/products/voip-telephone-system-integration/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-sip-client-python',
-        angular: 'https://github.com/ondewo/ondewo-sip-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-sip-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-sip-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-sip-client-js',
-      },
-      versions: [
-        { number: '4.0.0', link: 'ondewo-sip-api/4.0.0/' },
-        { number: '3.3.0', link: 'ondewo-sip-api/3.3.0/' },
-        { number: '3.2.0', link: 'ondewo-sip-api/3.2.0/' },
-      ],
-      end: '',
-    },
-    {
-      name: 'VTSI',
-      product_link:
-        'https://ondewo.com/en/products/voip-telephone-system-integration/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-vtsi-client-python',
-        angular: 'https://github.com/ondewo/ondewo-vtsi-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-vtsi-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-vtsi-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-vtsi-client-js',
-      },
-      versions: [
-        { number: '5.0.0', link: 'ondewo-vtsi-api/5.0.0/' },
-        { number: '4.0.0', link: 'ondewo-vtsi-api/4.0.0/' },
-      ],
-      end: '',
-    },
-    {
-      name: 'CSI',
-      product_link:
-        'https://ondewo.com/en/products/voip-telephone-system-integration/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-csi-client-python',
-        angular: 'https://github.com/ondewo/ondewo-csi-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-csi-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-csi-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-csi-client-js',
-      },
-      versions: [{ number: '3.0.0', link: 'ondewo-csi-api/3.0.0/' }],
-      end: '',
-    },
-    {
-      name: 'SURVEY',
-      product_link: 'https://ondewo.com/en/products/call-centre-ai/',
-      clients: {
-        python: 'https://github.com/ondewo/ondewo-survey-client-python',
-        angular: 'https://github.com/ondewo/ondewo-survey-client-angular',
-        nodejs: 'https://github.com/ondewo/ondewo-survey-client-nodejs',
-        typescript: 'https://github.com/ondewo/ondewo-survey-client-typescript',
-        js: 'https://github.com/ondewo/ondewo-survey-client-js',
-      },
-      versions: [{ number: '2.0.0', link: 'ondewo-survey-api/2.0.0/' }],
-      end: '',
-    },
-  ],
-  logos: {
-    python_logo:
-      'https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/files/python-logo-only.svg',
-    angular_logo: 'https://angular.io/assets/images/logos/angular/angular.svg',
-    typescript_logo: 'https://cdn-icons-png.flaticon.com/512/5968/5968381.png',
-    nodejs_logo:
-      'https://t3.ftcdn.net/jpg/03/52/67/82/240_F_352678266_NFcwIwhhY76mkQItT4lCxyxcCTP3LgvY.jpg',
-    js_logo: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png',
-  },
 };
 
-export { ondewo };
+export {ondewo};
