7e0cf7703733d7062e906748e7dd9ab4861cb3fd
Andreas Rath <andreas.rath@ondewo.com>
2024-04-18 07:02:48 +0200

    Added s2t Documentation for 5.7.0

--- data.js
+++ data.js
@@ -48,6 +48,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-s2t-client-js',
       },
       versions: [
+        { number: '5.7.0', link: 'ondewo-s2t-api/5.7.0/' },
         { number: '5.6.0', link: 'ondewo-s2t-api/5.6.0/' },
         { number: '5.5.0', link: 'ondewo-s2t-api/5.5.0/' },
         { number: '5.4.0', link: 'ondewo-s2t-api/5.4.0/' },
