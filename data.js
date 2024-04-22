388d149f5262722bbf5a90d5174118922581061c
Andreas Rath <andreas.rath@ondewo.com>
2024-04-22 10:06:25 +0200

    Added csi Documentation for 4.0.0

--- data.js
+++ data.js
@@ -135,6 +135,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-csi-client-js',
       },
       versions: [
+        { number: '4.0.0', link: 'ondewo-csi-api/4.0.0/' },
         { number: '3.2.0', link: 'ondewo-csi-api/3.2.0/' },
         { number: '3.1.0', link: 'ondewo-csi-api/3.1.0/' },
         { number: '3.0.0', link: 'ondewo-csi-api/3.0.0/' },
