a563aaad99082d74fcc3506fe5eab5195d90e06b
Andreas Rath <andreas.rath@ondewo.com>
2024-11-19 10:28:24 +0100

    Added vtsi Documentation for 8.0.0

--- data.js
+++ data.js
@@ -114,6 +114,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-vtsi-client-js',
       },
       versions: [
+        { number: '8.0.0', link: 'ondewo-vtsi-api/8.0.0/' },
         { number: '7.0.0', link: 'ondewo-vtsi-api/7.0.0/' },
         { number: '6.9.0', link: 'ondewo-vtsi-api/6.9.0/' },
         { number: '6.8.0', link: 'ondewo-vtsi-api/6.8.0/' },
