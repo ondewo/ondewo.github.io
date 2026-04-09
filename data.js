9580f875783562051e1421a4c3690d258aee7cab
Alexander Strasser <alexander.strasser@ondewo.com>
2026-04-09 14:54:39 +0200

    Added nlu Documentation for 6.5.0

--- data.js
+++ data.js
@@ -12,6 +12,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-nlu-client-js',
       },
       versions: [
+        { number: '6.5.0', link: 'ondewo-nlu-api/6.5.0/' },
         { number: '6.4.0', link: 'ondewo-nlu-api/6.4.0/' },
         { number: '6.3.0', link: 'ondewo-nlu-api/6.3.0/' },
         { number: '6.2.0', link: 'ondewo-nlu-api/6.2.0/' },
