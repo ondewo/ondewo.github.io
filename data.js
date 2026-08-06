6c77be28fff427274db7f9a0000da0f999e4430b
Andreas Rath <andreas.rath@ondewo.com>
2026-08-06 09:09:50 +0200

    Added nlu Documentation for 7.0.0

--- data.js
+++ data.js
@@ -12,6 +12,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-nlu-client-js',
       },
       versions: [
+        { number: '7.0.0', link: 'ondewo-nlu-api/7.0.0/' },
         { number: '6.13.0', link: 'ondewo-nlu-api/6.13.0/' },
         { number: '6.12.0', link: 'ondewo-nlu-api/6.12.0/' },
         { number: '6.11.0', link: 'ondewo-nlu-api/6.11.0/' },
