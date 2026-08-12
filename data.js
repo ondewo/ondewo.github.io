a493ac16f684e9b0b2b8cdcf786c6077a12e5352
Andreas Rath <andreas.rath@ondewo.com>
2026-08-12 13:26:48 +0200

    Added sip Documentation for 5.4.0

--- data.js
+++ data.js
@@ -117,6 +117,7 @@ const ondewo = {
         js: 'https://github.com/ondewo/ondewo-sip-client-js',
       },
       versions: [
+        { number: '5.4.0', link: 'ondewo-sip-api/5.4.0/' },
         { number: '5.3.0', link: 'ondewo-sip-api/5.3.0/' },
         { number: '5.2.0', link: 'ondewo-sip-api/5.2.0/' },
         { number: '5.1.0', link: 'ondewo-sip-api/5.1.0/' },
